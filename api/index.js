8d76a5599550bc17cc6cafe9afd2183ebc143883
Dwayne Paisley-Marshall <superdwayne@gmai.com>
2024-10-17 20:30:53 +0200

    Added new USDZ files hosted by github

--- api/index.js
+++ api/index.js
@@ -8,17 +8,17 @@ app.get('/api/models', (req, res) => {
         {
             name: "Jacket",
             glbUrl: "https://storage.polycam.io/captures/325eb391-c72a-431d-b304-98ddedef574a/raw.glb?t=1729077591034",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/jacket.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/goose.usdz"
         },
         {
             name: "Balenciaga",
             glbUrl: "https://storage.polycam.io/captures/d3efbdcb-ef62-4112-b7c5-3f099f24b636/raw.glb?t=1729077279389",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/balenciaga.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/used_new.usdz"
         },
         {
             name: "Fila",
             glbUrl: "https://storage.polycam.io/captures/e7860688-4e56-44e4-8b5a-d1fbcc34775f/raw.glb?t=1729077484936",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/fila.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/fila.usdz"
         },
         {
             name: "Nike",
@@ -28,12 +28,12 @@ app.get('/api/models', (req, res) => {
         {
             name: "Bag",
             glbUrl: "https://storage.polycam.io/captures/1ac77d1b-3178-4738-bbb4-ae1e6c883542/raw.glb?t=1729077815091",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/bag.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/nike.usdz"
         },
         {
             name: "Dior",
             glbUrl: "https://storage.polycam.io/captures/d330c813-0232-4620-b394-642145be55eb/raw.glb?t=1729077933033",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/dior.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/dior.usdz"
         }
     ];
     res.json(models);
@@ -41,32 +41,38 @@ app.get('/api/models', (req, res) => {
 
 // API endpoint to get a specific 3D model by name (GLB and USDZ)
 app.get('/api/models/:name', (req, res) => {
-    const models = {
-        "jacket": {
+    const models = [
+        {
+            name: "Jacket",
             glbUrl: "https://storage.polycam.io/captures/325eb391-c72a-431d-b304-98ddedef574a/raw.glb?t=1729077591034",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/jacket.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/goose.usdz"
         },
-        "balenciaga": {
+        {
+            name: "Balenciaga",
             glbUrl: "https://storage.polycam.io/captures/d3efbdcb-ef62-4112-b7c5-3f099f24b636/raw.glb?t=1729077279389",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/balenciaga.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/used_new.usdz"
         },
-        "fila": {
+        {
+            name: "Fila",
             glbUrl: "https://storage.polycam.io/captures/e7860688-4e56-44e4-8b5a-d1fbcc34775f/raw.glb?t=1729077484936",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/fila.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/fila.usdz"
         },
-        "nike": {
+        {
+            name: "Nike",
             glbUrl: "https://storage.polycam.io/captures/7dbfb890-bc2c-4b77-87b5-187973bcbdee/raw.glb?t=1729077764551",
             usdzUrl: "https://immersive-commerce.vercel.app/models/nike.usdz"
         },
-        "bag": {
+        {
+            name: "Bag",
             glbUrl: "https://storage.polycam.io/captures/1ac77d1b-3178-4738-bbb4-ae1e6c883542/raw.glb?t=1729077815091",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/bag.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/nike.usdz"
         },
-        "dior": {
+        {
+            name: "Dior",
             glbUrl: "https://storage.polycam.io/captures/d330c813-0232-4620-b394-642145be55eb/raw.glb?t=1729077933033",
-            usdzUrl: "https://immersive-commerce.vercel.app/models/dior.usdz"
+            usdzUrl: "https://superdwayne.github.io/usdzhost/dior.usdz"
         }
-    };
+    ];
 
     const modelName = req.params.name.toLowerCase();
     const modelData = models[modelName];
